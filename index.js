761c7d82d5aba84de19616ab4b9c065e96781b65
nmpellecchia <56317370+nmpellecchia@users.noreply.github.com>
2020-01-23 15:42:28 -0300

    Add functionality
    First version of the completed game. It's now possible to play

--- index.js
+++ index.js
@@ -40,6 +40,65 @@ function manejarRonda() {
         actualizarEstado('Su turno');
         desbloquearInputUsuario();
     }, RETRASO_TURNO_JUGADOR);
-    
 
+    secuenciaUsuario = [];
+    actualizarNumeroRonda(ronda++);
+    
+};
+
+function actualizarEstado(estado) {
+    document.querySelector('#estado').textContent = estado;
+};
+
+function actualizarNumeroRonda(numeroRonda) {
+    document.querySelector('#numero-ronda').textContent = numeroRonda
+};
+
+function bloquearInputUsuario() {
+    const $cuadros = document.querySelectorAll('.cuadro');
+
+    $cuadros.forEach(function($cuadro) {
+        $cuadro.onclick = function() {};
+    });
+};
+
+function seleccionarCuadroAleatorio () {
+    const $cuadros = document.querySelectorAll('.cuadro');
+    const index = Math.floor(Math.random() * $cuadros.length)
+    return $cuadros[index];
+};
+
+function resaltar($cuadro) {
+    $cuadro.style.opacity = 1;
+
+    setTimeout(function() {
+        $cuadro.style.opacity = 0.5;
+    }, 500);
+};
+
+function desbloquearInputUsuario() {
+    const $cuadros = document.querySelectorAll('.cuadro');
+
+    $cuadros.forEach(function($cuadro){
+        $cuadro.onclick = manejarInputUsuario;
+    });
+};
+
+function manejarInputUsuario(event) {
+    const $cuadro = event.target;
+    resaltar($cuadro);
+
+    secuenciaUsuario.push($cuadro);
+
+    const cuadroMaquina = secuenciaMaquina[secuenciaUsuario.length - 1];
+
+    if($cuadro.id !== cuadroMaquina.id) {
+        actualizarEstado('¡Perdiste! Dale a comenzar para jugar otra vez');
+        bloquearInputUsuario();
+    };
+
+    if(secuenciaUsuario.length === secuenciaMaquina.length) {
+        bloquearInputUsuario()
+        setTimeout(manejarRonda, 1000);
+    };
 };
